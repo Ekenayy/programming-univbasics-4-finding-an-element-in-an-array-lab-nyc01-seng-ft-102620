175fb7ebdd82e4520fae1d7ccc0d2203698ae063
Ekene N <ekenayy@gmail.com>
2020-10-06 03:13:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,7 +5,6 @@ def find_element_index(array, value_to_find)
   while value_to_find != array[counter] do
   break if array.includes?(value_to_find) == false
   counter += 1
-    return index(value_to_find)
   end
 end
 
